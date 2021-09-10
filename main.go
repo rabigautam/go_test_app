b7f190a2733f18ed482782c00f244e0a3af5a52a
Rabi Gautam <rabigautam43@gmail.com>
2021-09-10 14:19:02 +0545

    implemented package scope and refactor

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	// "fmt"
-	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"strings"
-
-	"io"
+	env "github.com/joho/godotenv"
 	"log"
-	"net/http"
-	"net/smtp"
 	"os"
-
-	"github.com/joho/godotenv"
+	"strings"
 )
 
 /**
@@ -21,7 +13,7 @@ import (
  */
 
 func envVariable(key string) string {
-	err := godotenv.Load(".env")
+	err := env.Load(".env")
 	if err != nil {
 		log.Fatalf("Error occurs while loading .env")
 	}
@@ -64,101 +56,22 @@ func main() {
 
 	fmt.Println("from :" + from)
 
-	templateUrl := baseUrl + "template/"
-	body, err := doGetRequest(templateUrl, token)
-	//request to given url
+	required_template, err := getTemplate(baseUrl, token)
 
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatalln(err.Error())
 	}
-	var templateResponse TemplateResponse
-	json.Unmarshal(body, &templateResponse)
-	result := templateResponse.Result
+	fmt.Println(required_template, "required template")
 
-	var required_template Template
-
-	for _, value := range result {
-
-		if value.Template_id == "17" {
-			// Found!
-			required_template = value
-			break
-		}
-	}
-	fmt.Println(required_template.Template_html)
-
-	contactUrl := baseUrl + "contact/?limit=11"
-	body, err = doGetRequest(contactUrl, token)
-	//request to given url
-
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	var contactResponse ContactResponse
-	json.Unmarshal(body, &contactResponse)
-	resultContact := contactResponse.Result
+	required_contact, err := getContact(baseUrl, token)
+	resultContact := required_contact.Result
 
 	for key, contact := range resultContact {
 		fmt.Println(key, "_", contact)
 		msg := strings.ReplaceAll(required_template.Template_text, "{{contact_name}}", contact.Contact_name)
-		fmt.Println(msg,"msg")
+		fmt.Println(msg, "msg")
+
 		sendMail(from, password, []string{contact.Contact_name}, msg)
 	}
 
-}
-
-func doGetRequest(url string, token string) ([]byte, error) {
-	//request to given url
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatalf(err.Error())
-		return nil, err
-	}
-
-	//add headers to request
-
-	req.Header.Add("Authorization", token)
-	req.Header.Add("Content_type", "application/json")
-
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Fatalf(err.Error())
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-
-	return body, err
-}
-
-// Sending Email Using Smtp in Golang
-
-// Main function
-func sendMail(from string, password string, toList []string, msg string) {
-
-	host := "smtp.gmail.com"
-
-	// Its the default port of smtp server
-	port := "587"
-
-	body := []byte(msg)
-
-	// to act as username.
-	auth := smtp.PlainAuth("", from, password, host)
-
-	err := smtp.SendMail(host+":"+port, auth, from, toList, body)
-
-	// handling the errors
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Successfully sent mail to all user in toList")
 }
